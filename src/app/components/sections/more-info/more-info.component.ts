ab94527b717dd875c67d97aa4ef3b7f8a93441f3
ae-tafur <ae.tafur@hotmail.com>
2026-02-09 21:47:01 -0500

    refactor: nan-link color and faqs

--- src/app/components/sections/more-info/more-info.component.ts
+++ src/app/components/sections/more-info/more-info.component.ts
@@ -16,7 +16,7 @@ export class MoreInfoComponent {
   faqs = [
     {
       id: 'One',
-      question: '¿Puedo renunciar a mi opción de grado?',
+      question: '¿En qué puede trabajar un microbiólogo?',
       answer: 'Dado que es un requisito de grado tomar una opción de grado no es posible renunciar tu opción de ' +
         'grado. Sin embargo, el acuerdo 005 de 12 de febrero de 2025 establece que es posible realizar el cambio ' +
         'de opción de grado por una unica vez. Es importante que te mantengas informado sobre las fechas para cada ' +
@@ -24,7 +24,7 @@ export class MoreInfoComponent {
     },
     {
       id: 'Two',
-      question: 'Ya envié la solicitud usando el formulario ¿cuando recibo respuesta?',
+      question: '¿Qué requisitos necesito para graduarme?',
       answer: 'El comité de investigación del programa y de la facultad revisará las solicitudes en la sesión ' +
         'programada según ciclo. Revisa el cronograma de recepción y evaluación de solicitudes para conocer las ' +
         'fechas específicas según el ciclo en el que enviaste tu solicitud. Una vez evaluada tu solicitud, ' +
@@ -33,7 +33,7 @@ export class MoreInfoComponent {
     },
     {
       id: 'Three',
-      question: '¿Puedo cambiar el título u objetivos de mi trabajo de grado?',
+      question: '¿Necesito acreditar inglés?',
       answer: 'Si, puedes hacerlo. Sin embargo, es importante que tengas en cuenta que esto debe ser informado al ' +
         'comité de investigación del programa y de la facultad. Cualquier cambio en el título u objetivos debe ser ' +
         'justificado y aprobado por el comité para asegurar que se mantenga la coherencia con los requisitos ' +
@@ -42,7 +42,17 @@ export class MoreInfoComponent {
     },
     {
       id: 'Four',
-      question: 'Ya finalicé mi trabajo de grado ¿Qué debo hacer?',
+      question: '¿Hay un puntaje mínimo en las pruebas Saber Pro?',
+      answer: `Una vez hayas finalizado tu trabajo de grado, debes seguir estos pasos: <ol>
+            <li>Revisa que tu trabajo cumpla con todos los requisitos establecidos en el acuerdo 005 de 12 de febrero de 2025.</li>
+            <li>Prepara tu trabajo de grado según las normas de presentación y formato requeridas por el programa.</li>
+            <li>Envia el formulario web de solicitud de opción de grado, adjuntando tu trabajo de grado y cualquier documento adicional requerido. Revisa la sección opciones de grado para mas detalles.</li>
+            <li>Espera la respuesta por parte del comité de investigación del programa y de la facultad. Recibirás una notificación por correo electrónico indicando si tu trabajo ha sido aprobado y los pasos a seguir para la sustentación.</li>
+          </ol>`
+    },
+    {
+      id: 'Five',
+      question: '¿Donde puedo realizar mis practicas?',
       answer: `Una vez hayas finalizado tu trabajo de grado, debes seguir estos pasos: <ol>
             <li>Revisa que tu trabajo cumpla con todos los requisitos establecidos en el acuerdo 005 de 12 de febrero de 2025.</li>
             <li>Prepara tu trabajo de grado según las normas de presentación y formato requeridas por el programa.</li>
